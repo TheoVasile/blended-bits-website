18ff6930fd894cead72bc15d45bc8f3daf3b37ac
TheoVasile <theo.vasile50@yahoo.com>
2021-07-22 23:32:02 -0400

    switched to react framework

---
+++ src/Home.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <body>
+            <div class="hero-bg">
+                <h1>Take your 3D renders to<br></br> the next level</h1>
+                <p>3D assets for any scene</p>
+            </div>
+            <div class="parallax">
+                <section class="info">
+                    <h2>Blended Bits provides the most expansive collection of professional-grade assets for any scene</h2>
+                    <h2>Our focus is on letting 3D artists unleash their full creativity without the hassle of creating secondary assets</h2>
+                </section>
+            </div>
+            <div class="comparison">
+                <section class="advertisment">
+                    <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h2>
+                </section>
+            </div>
+            <div class="collections">
+                <section class="title">
+                    <h1>OUR MOST POPULAR COLLECTIONS</h1>
+                    <p>Lorem ipsum</p>
+                </section>
+            </div>
+        </body>
+    )
+}
+
+export default Home
---
+++ src/components/navbar/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+    {
+        title: "Home",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "Sign In",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "Office",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "Bathroom",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "Kitchen",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "All",
+        url: "#",
+        cName: "nav-links"
+    }
+]
